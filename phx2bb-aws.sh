78d68360a25cd24f0fe9b545359fa64f17e4597b
DOH-JDJ0303 <ubuntu@ip-172-31-12-251.us-west-2.compute.internal>
2023-10-20 17:55:41 +0000

    script for creating BigBacter manifest from PHoeNIx output

---
+++ phx2bb-aws.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+path=${1%/}
+
+aws s3 cp ${path}/Phoenix_Summary.tsv ./
+
+echo "sample,taxa,assembly,fastq_1,fastq_2" > manifest.csv
+
+for i in $(cat Phoenix_Summary.tsv | tr '\t' '@' | tr ' ' '_')
+do
+    id=$(echo ${i} | cut -f 1 -d '@')
+    qc=$(echo ${i} | cut -f 2 -d '@')
+    taxa=$(echo ${i} | cut -f 9 -d '@')
+
+    echo -e "${id}\t${qc}\t${taxa}"
+
+    if [[ ${qc} == "PASS"  ]]
+    then
+        echo "${id},${taxa},${path}/assembly/${id}.scaffolds.fa.gz,${path}/reads/${id}_1.trim.fastq.gz,${path}/reads/${id}_2.trim.fastq.gz" >> manifest.csv
+    fi
+done
